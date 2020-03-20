05af0ca350ccf419c25e336ae62cf335b49ba867
MichaelObvious <michele.palese42@outlook.it>
2020-03-20 14:30:33 +0100

    array (and drawing)

---
+++ src/array.rs
@@ -0,0 +1,44 @@
+use console_gfx::rendering::{ colour::Colour, renderer::RenderCommand };
+use std::ops;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Array {
+    array: Vec<u32>,
+    i:     usize,
+    j:     usize,
+}
+
+impl Array {
+    
+    pub fn new(width: u32) -> Array {
+        Array { array: (1..width + 1).collect(), i: 0, j: 0 }
+    }
+
+    pub fn len(&self) -> usize {
+        self.array.len()
+    }
+
+    pub fn render(&self, height: usize) -> Vec<RenderCommand> {
+        let mut commands = Vec::new();
+        commands.append(&mut vec![
+            RenderCommand::SetBackground(Colour::black()),
+            RenderCommand::SetColour(Colour::white()),
+        ]);
+        for x in 0..self.len() {
+            commands.push(RenderCommand::DrawLine(x, height - 1, x, height - (self.array[x] as f64 / self.len()as f64 * height as f64).ceil() as usize, '|'));
+        }
+
+        return commands;
+    }
+
+}
+
+impl ops::Index<usize> for Array {
+    
+    type Output = u32;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.array[i]
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate console_gfx;
+
+mod algorithms;
+mod array;
+
+use console_gfx::rendering::{ colour::Colour, renderer::{ RenderCommand, Renderer } };
+use std::{ thread, time };
+
 fn main() {
-    println!("Hello, world!");
+    let width = 20;
+    let height = 20;
+    let ups = 20;
+    let step = time::Duration::from_millis(1000 / ups);
+
+    let arr = array::Array::new(width as u32);
+
+    let mut r = Renderer::new((width, height));
+    r.push_cmds(vec![
+        RenderCommand::SetBackground(Colour::rgb(10, 10, 20)),
+        RenderCommand::SetColour(Colour::white()),
+        RenderCommand::DrawBorder('#'),
+    ]);
+
+    let mut last_time = time::Instant::now();
+    loop {
+        r.push_cmds(arr.render(height));
+        r.update();
+
+        thread::sleep(get_sleep_time(&last_time, &step));
+        last_time = time::Instant::now();
+    }
+
+    r.push_cmd(RenderCommand::Reset);
+    r.update();
+
+    ()
+}
+
+fn get_sleep_time(last: &time::Instant, step: &time::Duration) -> time::Duration {
+    let elapsed = last.elapsed();
+    if *step < elapsed {
+        return time::Duration::from_millis(0);
+    } else {
+        return *step - elapsed;
+    }
 }
